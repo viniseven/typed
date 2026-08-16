669538e0ba0c7f3046dcdaaf0abf3a3b416e7a9e
Marcus Vinicius <marcusv892@gmail.com>
2026-08-16 14:17:44 -0300

    refactor:  removing router first in file and add routes and express json

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from "express";
+import userRoutes from "./routes/userRoutes.ts";
 
-const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Página Home");
-});
+const app = express();
+
+app.use(express.json());
+app.use(userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
